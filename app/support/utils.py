d8cb6df1e74601c6508d54b5128f8f0402fc4da3
Rodolfo De Nadai <rdenadai@gmail.com>
2022-12-04 08:24:35 -0300

    feat: Submit code to AoC site;

--- app/support/utils.py
+++ app/support/utils.py
@@ -1,10 +1,26 @@
+# Some regex parts taken from: https://github.com/anthonywritescode/aoc2022 (MIT License)
+
 import os
+import re
 from functools import wraps
 from os.path import abspath, dirname
 from time import perf_counter
 from typing import Any, Callable, List, Optional, Tuple
 
 import httpx
+
+TOO_QUICK = re.compile("You gave an answer too recently.*to wait.")
+WRONG = re.compile(r"That's not the right answer.*?\.")
+RIGHT = "That's the right answer!"
+ALREADY_DONE = re.compile(r"You don't seem to be solving.*\?")
+
+
+def _get_token() -> str:
+    root_dir = dirname(abspath(__file__))
+    token: str = "session="
+    with open(f"{root_dir}/../../.env", "r", encoding="utf-8") as file:
+        token = file.read().strip()
+    return token
 
 
 class InputDownloader:
@@ -19,16 +35,9 @@ class InputDownloader:
         with open(filename, "w", encoding="utf-8") as file:
             file.write(self._download())
 
-    def _get_token(self) -> str:
-        root_dir = dirname(abspath(__file__))
-        token: str = "session="
-        with open(f"{root_dir}/../../.env", "r", encoding="utf-8") as file:
-            token = file.read().strip()
-        return token
-
     def _download(self) -> str:
         with httpx.Client() as client:
-            headers = {"Cookie": self._get_token()}
+            headers = {"Cookie": _get_token()}
             req = client.get(
                 f"https://adventofcode.com/{self.year}/day/{self.day}/input",
                 follow_redirects=True,
@@ -37,6 +46,45 @@ class InputDownloader:
             if req.status_code == 200:
                 return req.text
         return ""
+
+
+class InputSubmit:
+    def __init__(self, day: int, year: int = 2022) -> None:
+        self.day = day
+        self.year = year
+
+    def submit(self, part: int, answer: str):
+        filename = f"app/day{self.day:02}/.submit_part_{part}.txt"
+        if os.path.exists(filename):
+            return "Already submitted"
+
+        with httpx.Client() as client:
+            headers = {"Cookie": _get_token()}
+            req = client.post(
+                f"https://adventofcode.com/{self.year}/day/{self.day}/answer",
+                data={"level": part, "answer": answer},
+                follow_redirects=True,
+                headers=headers,
+            )
+            correct, text = self._parse_answer(req.text)
+
+        if correct:
+            with open(filename, "w", encoding="utf-8") as file:
+                file.write(f"Part {part} answer: {answer}")
+
+        return text
+
+    def _parse_answer(self, contents: str) -> Tuple[bool, str]:
+        for error_regex in (WRONG, TOO_QUICK):
+            error_match = error_regex.search(contents)
+            if error_match:
+                return False, error_match[0]
+        error_match = ALREADY_DONE.search(contents)
+        if error_match:
+            return True, error_match[0]
+        if RIGHT in contents:
+            return True, RIGHT
+        return False, "Unknown error"
 
 
 def parse_module_to_day(module_name: str = "") -> Tuple[str, int]:
